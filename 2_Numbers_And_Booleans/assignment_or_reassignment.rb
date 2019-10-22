ae74837d16d2e0e1c115deb5781dde236349dbe8
James Saunders <jtsgithub@gmail.com>
2019-10-22 18:04:47 +0100

    Commit

--- 2_Numbers_And_Booleans/assignment_or_reassignment.rb
+++ 2_Numbers_And_Booleans/assignment_or_reassignment.rb
@@ -2,5 +2,43 @@ a = 10
 p a
 
 # a = a + 5 is the same as the line below
+
+a = a + 5
+p a
+
 a += 5
 p a
+
+
+b = 100
+p b
+
+# b = b - 50
+b -= 50
+p b
+
+c = 3
+p c
+
+#c = c * 4
+c *= 4
+p c
+
+puts
+
+d = 10
+p d
+
+d /= 5
+p d
+
+e = 10
+
+e %= 3 #Modulo (remainder)
+p e
+
+f = 2
+p f
+
+f **= 3 #To the power of
+p f
