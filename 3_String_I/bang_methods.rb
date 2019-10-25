59bba444228c820dfa9970b669ca2b6562f2ad2c
James Saunders <jtsgithub@gmail.com>
2019-10-25 10:46:16 +0100

    bang_method commit

---
+++ 3_String_I/bang_methods.rb
@@ -0,0 +1,16 @@
+
+word = "hello"
+p word.capitalize #Doesn't save it
+p word
+
+puts
+
+again = "hello"
+again = again.capitalize
+p again
+
+
+#Below is the bang method. The ! at the end overwrites the variable
+name = "jAMEs"
+p name.capitalize!
+p name
