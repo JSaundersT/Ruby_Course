d9825b3d95aeb77eb5fd94b2e26e2e90236cc553
James Saunders <jtsgithub@gmail.com>
2019-10-27 09:36:16 +0000

    Return Values

---
+++ 4_Methods_and_Conditions/return_values_I.rb
@@ -0,0 +1,18 @@
+
+def add_two_numbers(num1, num2)
+  puts "Ok, lets add these two numbers together"
+  return num1 + num2
+end
+
+p add_two_numbers(4, 5)
+p add_two_numbers(3, 15)
+
+puts
+def add_three_numbers(num1, num2, num3)
+  puts "Ok, lets add these three numbers together"
+  num1 + num2 + num3
+  puts "Thanks"
+end
+
+p add_three_numbers(4, 5, 9)
+p add_three_numbers(3, 15, 5)
---
+++ 4_Methods_and_Conditions/return_values_II.rb
@@ -0,0 +1,14 @@
+def return_string
+  "What will be the return value here"
+end
+
+def return_guess
+  puts "Blah, Blah, Blah"
+  print "What will be the return value here"
+end
+
+p return_string
+puts
+result = return_guess
+p result
+p result.class
