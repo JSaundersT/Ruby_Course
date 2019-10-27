bde9e9e0838ce86dea634a99c5f380ed7ece22e5
James Saunders <jtsgithub@gmail.com>
2019-10-27 08:59:58 +0000

    Method Parameters

---
+++ 4_Methods_and_Conditions/Method_parameters.rb
@@ -0,0 +1,11 @@
+
+def praise_person(name, age, gender)
+  puts "#{name} is amazing"
+  puts "#{name} is funny"
+  puts "#{name} is the best"
+  puts "#{gender} is #{age} years old!"
+end
+
+praise_person("James", 29, "He")
+puts
+praise_person("Hagrid", 24, "She")
