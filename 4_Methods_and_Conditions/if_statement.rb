b98c4e12284063096b149788e8b9033c10ca55f9
James Saunders <jtsgithub@gmail.com>
2019-10-28 15:21:33 +0000

    If Statements & Truthiness

---
+++ 4_Methods_and_Conditions/if_statement.rb
@@ -0,0 +1,30 @@
+
+if 5 < 7
+  puts "What maths statement is true - 5 and 7"
+end
+
+
+if 2 > 6
+  puts "What maths statement is true - 2 and 6"
+end
+
+
+puts
+
+password = "topsecret"
+
+if password == "topsecret"
+  puts "Congratulations, you've logged into our system"
+end
+
+word = "Kangaroo"
+
+if word.length == 8
+  puts "That's right!"
+end
+
+puts
+
+if word.include?("roo")
+  puts "Roo!!!!"
+end
---
+++ 4_Methods_and_Conditions/truthiness_falsiness.rb
@@ -0,0 +1,12 @@
+
+if 5
+  puts "That is true"
+end
+
+if nil
+  puts "Hello"
+end
+
+if 5..19
+  puts "Range is true"
+end
