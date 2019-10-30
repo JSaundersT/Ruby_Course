9c02347b2b6ad4a55e19ad64b0de1a182d289123
James Saunders <jtsgithub@gmail.com>
2019-10-30 16:54:15 +0000

    Default_perameters.rb

---
+++ 4_Methods_and_Conditions/default_or_operational_parameters.rb
@@ -0,0 +1,7 @@
+def make_phone_call(number, international_code = 44, area_code = 207)
+  puts "I am going to call the number +#{international_code}-#{area_code}-#{number}"
+end
+
+make_phone_call(2475747, 1, 203)
+
+make_phone_call(1234567, 34, 222)
