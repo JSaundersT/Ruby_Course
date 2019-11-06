a685f9ec253c177a90eaba7fc199dc5306e3ea92
James Saunders <jtsgithub@gmail.com>
2019-11-06 20:44:12 +0000

    End of module 5

---
+++ 5_Methods_and_Conditions_II/conditional_assignment.rb
@@ -0,0 +1,18 @@
+# y = nil
+# p y
+#
+# y ||= 5
+# p y
+#
+# y ||= 10
+# p y
+
+
+
+greeting = "hello"
+extraction = 10
+letter = greeting[extraction] #H, e, l, l, o
+
+p letter
+letter ||= "Not found"
+p letter
---
+++ 5_Methods_and_Conditions_II/statement_modifiers.rb
@@ -0,0 +1,10 @@
+
+number = 5000
+
+if number > 2500
+  puts "Huge number!"
+end
+
+puts "Huge number!" if number > 2500
+
+#These can be written in the same way as above
