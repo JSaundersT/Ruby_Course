a1c60c1f197a0c09aacf36c5271d65d8577e09f4
James Saunders <jtsgithub@gmail.com>
2019-11-08 09:32:58 +0000

    End of the array module

--- 7_Arrays/first_and_last.rb
+++ 7_Arrays/first_and_last.rb
@@ -1,5 +1,15 @@
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr = [1, 3, 5, 7, 9, 15, 21, 18, 6]
 
-p arr.first(2)
-p arr.last(4)
+def custom_first(arr, num = 0)
+  return arr[0] if num == 0
+  arr[0, num]
+end
+
+p custom_first(arr)
+p custom_first(arr, 5)
+
+def custom_last(arr, num = 0)
+  return arr[-1] if num == 0
+  arr[-num..-1]
+end
---
+++ 7_Arrays/push_method_shovel.rb
@@ -0,0 +1,12 @@
+
+locations = ["School", "House", "Bar"]
+p locations
+
+# locations.push("Restaurant")
+# p locations
+#
+# locations << "Restaurant"
+# p locations
+
+locations.insert(1, "Restaurant")
+p locations
