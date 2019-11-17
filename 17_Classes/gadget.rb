be2382cf5b6205b83355d2e0bbff851757d82cd2
James Saunders <jtsgithub@gmail.com>
2019-11-17 23:34:54 +0000

    next

--- 17_Classes/gadget.rb
+++ 17_Classes/gadget.rb
@@ -1,10 +1,18 @@
 class Gadget
+  def initialize
+    @username = "User #{rand(1..100)}"
+    @password = "topsecret"
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
+  def info
+    "Gadget #{@production_number} has a username of #{@username}"
+  end
+
+
 end
 
-shiny = Gadget.new
-flashy = Gadget.new
+phone = Gadget.new
+laptop = Gadget.new
 
-glossy = shiny
-
-p glossy.object_id
-p shiny.object_id
+puts phone.info
+puts laptop.info
